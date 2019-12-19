0f3d22edb0fda93b0fd09791d0b6721fdd49367a
Himanshu Chopra <himanchops@gmail.com>
2019-12-20 01:05:29 +0530

    Authentication and Flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,34 @@
 class SessionsController < ApplicationController
 
+	before_action :loggin_in_redirect, only: [:new, :create]
+
 	def new
+	end
+
+	def create
+		user = User.find_by(username: params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "You have successfully logged in"
+			redirect_to root_path
+		else
+			flash.now[:error] = "Incorrect username or password"
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have logged out."
+		redirect_to login_path
+	end
+
+	private
+
+	def loggin_in_redirect
+		if logged_in?
+			flash[:error] = "You are already logged in"
+			redirect_to root_path
+		end
 	end
 end
